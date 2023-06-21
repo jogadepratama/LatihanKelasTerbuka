6c6739d6fe9089d7471b2569a387a47036403b5f
Gojes <jogadepratama@gmail.com>
2023-06-21 21:58:29 +0700

    done dari kos

--- LatihanDictOp.py
+++ LatihanDictOp.py
@@ -5,14 +5,34 @@ data_dict = {
     'tong':'otong surotong',
     'dung':'dudung marudung'
 }
+
 #panjang dictionary
 LENDICT = len(data_dict)
 print(f'panjang dictionary : {LENDICT}')
+
 #mengecek key exist atau tidak
 KEY = 'cup'
 CHECKKEY = KEY in data_dict
 print(f'apakah {KEY} ada di data_dict: {CHECKKEY}')
+
 #mengakses value (read)
 print(data_dict["cup"])
 print(data_dict.get('cup'))
 print(data_dict.get('kis','key tidak ditemukan'))
+
+#mengupdate data dict
+data_dict['cup'] = "ucup si malam"
+print(data_dict)
+data_dict['sep'] = 'asep tukang kaset'
+print(data_dict)
+
+data_dict.update({"cup":"ucup surucup"})
+print(data_dict)
+data_dict.update({"jesgo":"gojes typing"})
+print(data_dict)
+
+#mendelet data dict
+del data_dict ["jesgo"]
+print(data_dict)
+
+
