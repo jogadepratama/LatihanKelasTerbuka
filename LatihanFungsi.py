c18c8cbe42b8227c4bcdfc4ea18d7d8170715e45
Gojes <jogadepratama@gmail.com>
2023-06-26 22:21:31 +0700

    Semangat Jesgo

---
+++ LatihanFungsi.py
@@ -0,0 +1,17 @@
+''' Membuat Fungsi '''
+
+def hello_world():
+    print("Hello World")
+    print("Terhadap Ucup")
+    print("Dan Dadang Suradang")
+
+hello_world()
+hello_world()
+
+# fungsi()
+
+def fungsi():
+    """ Pemanggilan Fungsi Harus Setelah Fungsi Itu Di Buat"""
+    print("Ini adalah print() dari fungsi yang dibuat")
+
+fungsi()
---
+++ LatihanFungsiArg.py
@@ -0,0 +1,21 @@
+''' Fungsi Argumen'''
+
+'''
+def nama_fungsi(argument):
+    Badan Fungsi
+
+'''
+
+def hello_world(name):
+    print(f'Selamat Datang Dunia {name}')
+
+hello_world("Ucup")
+hello_world("Asep")
+
+def tambah(angka1,angka2):
+    '''Fungsi Tambah'''
+    hasil = angka1 + angka2
+    print(f'{angka1} + {angka2} = {hasil}')
+
+tambah(5,6)
+tambah(584000,568441)
